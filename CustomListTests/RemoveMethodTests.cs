3a13a1ea9168706b797502995a39eab4388ba63d
Shea Raymond <shea.raymond01@gmail.com>
2023-08-31 18:26:00 -0500

    Adding 6 failing Remove method tests. Ready to begin TDD build of RemoveMethod.

--- CustomListTests/RemoveMethodTests.cs
+++ CustomListTests/RemoveMethodTests.cs
@@ -1,12 +1,116 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using CustomList;
 
 namespace CustomListTests
 {
-    internal class RemoveMethodTests
+
+    [TestClass]
+    public class RemoveMethodTests
     {
+       
+        [TestMethod]
+        public void Remove_ItemInArray_ItemIsRemovedFromArray()
+        {
+            // Arrange
+            CustomList<string> customList = new CustomList<string>();
+            customList.Add("a");
+            customList.Add("b");
+            customList.Add("c");
+            customList.Add("d");
+
+            // Act
+            customList.Remove("a");
+            // Assert
+            Assert.AreEqual(3, customList.Count);
+        }
+        
+        [TestMethod]
+        public void Remove_ItemRemovedSuccess_ReturnsTrue()
+        {
+            // Arrange
+            CustomList<string> customList = new CustomList<string>();
+            customList.Add("a");
+            customList.Add("b");
+            customList.Add("c");
+            customList.Add("d");
+            // Act
+            customList.Remove("a");
+            // Assert
+            Assert.IsTrue(customList.Count == 3);
+        }
+        
+        [TestMethod]
+        public void Remove_CountWhenTryingToRemoveItemNotInList_RemainsUnchanged()
+        {
+            // Arrange
+            CustomList<string> customList = new CustomList<string>();
+            customList.Add("a");
+            customList.Add("b");
+            customList.Add("c");
+            customList.Add("d");
+            // Act
+            customList.Remove("e");
+            // Assert
+            Assert.AreEqual(4, customList.Count);
+        }
+        [TestMethod]
+        public void Remove_IndexOfItemInIndex2WhenItemInIndex1Removed_ItemIsMovedToIndex1()
+        {
+            // Arrange
+            CustomList<string> customList = new CustomList<string>();
+            customList.Add("a");
+            customList.Add("b");
+            customList.Add("c");
+            customList.Add("d");
+            // Act
+            customList.Remove("b");
+            // Assert
+            Assert.AreEqual("c", customList.Items[1]);
+        }
+        [TestMethod]
+        public void Remove_IndexOfItemInIndex3WhenItemInIndex2Removed_ItemIsMovedToIndex2()
+        {
+            // Arrange
+            CustomList<string> customList = new CustomList<string>();
+            customList.Add("a");
+            customList.Add("b");
+            customList.Add("c");
+            customList.Add("d");
+            // Act
+            customList.Remove("b");
+            // Assert
+            Assert.AreEqual("d", customList.Items[2]);
+        }
+
+        [TestMethod]
+        public void Remove_MultipleInstancesOfItemCAfterRemovingC_OnlyFirstCRemovedOthersRemain()
+        {
+            // Arrange
+            CustomList<string> customList = new CustomList<string>();
+            customList.Add("a");
+            customList.Add("b");
+            customList.Add("c");
+            customList.Add("c");
+            customList.Add("c");
+            // Act
+            customList.Remove("c");
+            int c_Count = 0;
+            foreach (string item in customList.Items)
+            {
+                if (item == "c")
+                {
+                    c_Count++;
+                }
+            }
+            // Assert
+            Assert.AreEqual(2, c_Count);
+        }
     }
+
 }
