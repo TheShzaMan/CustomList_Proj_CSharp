ec48ebf5c07efa6ca481f2ddb073b5c75bd9a076
Shea Raymond <shea.raymond01@gmail.com>
2023-09-04 11:41:00 -0500

    Finishing unit tests for minus operator overload. Now building logic to pass the tests.

---
+++ CustomListTests/MinusOperatorOverloadTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomList;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class MinusOperatorOverloadTests
+    {
+        [TestMethod]
+        public void MinusOperator_SecondListSubtractedFromFirst_NewListOnlyContainsItemsthatWerentSubtracted()
+        {
+            // Arrange
+            CustomList<string> firstList = new CustomList<string>() { "a", "b", "c", "a", "b"};
+            CustomList<string> secondList = new CustomList<string>() { "b", "c", "a",};
+
+            // Act
+            CustomList<string> newList = firstList - secondList;
+            string itemsInNewList = newList.ToString();
+            // Assert
+            Assert.AreEqual("a b", itemsInNewList); 
+        }
+
+        [TestMethod]
+        public void MinusOperator_LargerSecondListSubtractedFromSmallerFirst_NewListOnlyContainsItemsthatWerentSubtracted()
+        {
+            // Arrange
+            CustomList<string> firstList = new CustomList<string>() { "a", "b", "c" };
+            CustomList<string> secondList = new CustomList<string>() { "b", "c", "d", "g", "e", "f"};
+
+            // Act
+            CustomList<string> newList = firstList - secondList;
+            string itemsInNewList = newList.ToString();
+            // Assert
+            Assert.AreEqual("a", itemsInNewList);
+        }
+
+        [TestMethod]
+        public void MinusOperator_SecondListIsBlankAndSubtracted_NewListSameAsFirstList()
+        { 
+            // Arrange
+            CustomList<string> firstList = new CustomList<string>() { "a", "b", "c", "a", "b" };
+            CustomList<string> secondList = new CustomList<string>() { "b", "c", "a", };
+
+            // Act
+            CustomList<string> newList = firstList - secondList;
+            string itemsInNewList = newList.ToString();
+            // Assert
+            Assert.AreEqual(firstList, newList);
+        }
+        [TestMethod]
+        public void MinusOperator_AnItemOnlySubtractsOneInstanceIfMultiplePresent_OnlyOneInstanceRemoved()
+        {
+            // Arrange
+            CustomList<string> firstList = new CustomList<string>() { "a", "a", "a"};
+            CustomList<string> secondList = new CustomList<string>() { "a"`
+ |};
+
+            // Act
+            CustomList<string> newList = firstList - secondList;
+            string itemsInNewList = newList.ToString();
+            // Assert
+            Assert.AreEqual(firstList, newList);
+        }
+    }
+}
